006b4dd92f3b063bcb5d0ee368113cd5216b02da
Unnop Sricharoenchai <tee@numthang.org>
2019-06-01 00:06:18 +0700

    author

--- plugins/numthang/homeschool/components/EvaluationForm.php
+++ plugins/numthang/homeschool/components/EvaluationForm.php
@@ -7,12 +7,16 @@ use Redirect;
 use Numthang\Homeschool\Models\Course;
 use Numthang\Homeschool\Models\Evaluation;
 use Flash;
+use RainLab\User\Models\User;
+use FireUnion\BlogFront\Models\Author;
+
 
 class EvaluationForm extends ComponentBase
 {
 
     public $record;
     public $course;
+    public $authors;
     public function componentDetails() {
         return [
             'name'        => 'Evaluation Form',
@@ -34,6 +38,11 @@ class EvaluationForm extends ComponentBase
           $this->record = Evaluation::find($this->param('id'));
         $this->course = Course::find($this->record->attributes['course_id']);
       }
+
+      foreach (Author::with('user')->get() as $user) {
+        $authors[$user->user->id] = $user->user->name.' '.$user->user->surname;
+  		}
+  		$this->authors = $authors;
 		}
     public function onSave() {
       $validator = Validator::make(
