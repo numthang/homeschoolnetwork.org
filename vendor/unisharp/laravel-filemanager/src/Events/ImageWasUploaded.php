0ac11b20867e476f074d3cff5510a49e2f8951be
Unnop Sricharoenchai <tee@numthang.org>
2019-08-27 10:27:13 +0700

    lfm config

--- vendor/unisharp/laravel-filemanager/src/Events/ImageWasUploaded.php
+++ vendor/unisharp/laravel-filemanager/src/Events/ImageWasUploaded.php
@@ -11,7 +11,9 @@ class ImageWasUploaded
     public function __construct($path)
     {
         $this->path = $path;
+        /*numthang add this method */
         $this->resizeAfterUploaded();
+        /*end numthang*/
     }
     public function resizeAfterUploaded() {
       $original_image = Image::make($this->path);
@@ -19,8 +21,8 @@ class ImageWasUploaded
       $original_height = $original_image->height();
 
       // FIXME size should be configurable
-      if ($original_width > 1200) {
-          $ratio = 1200 / $original_width;
+      if ($original_width > 1024) {
+          $ratio = 1024 / $original_width;
           $width = $original_width * $ratio;
           $height = $original_height * $ratio;
       } else {
@@ -28,8 +30,8 @@ class ImageWasUploaded
           $height = $original_height;
       }
 
-      if ($height > 900) {
-          $ratio = 900 / $original_height;
+      if ($height > 800) {
+          $ratio = 800 / $original_height;
           $width = $original_width * $ratio;
           $height = $original_height * $ratio;
       }
