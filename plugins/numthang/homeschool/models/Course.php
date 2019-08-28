188d80a6465fa8e4d18e9c57f367543232dd437f
Unnop Sricharoenchai <tee@numthang.org>
2019-08-28 20:57:51 +0700

    course improve

--- plugins/numthang/homeschool/models/Course.php
+++ plugins/numthang/homeschool/models/Course.php
@@ -24,7 +24,7 @@ class Course extends Model
     ];
 
     //กำหนด field ให้เป็น fillable
-    protected $fillable = ['user_id', 'hs_name', 'name', 'student_name', 'birth_date', 'school_exp', 'development', 'class_reason', 'purpose', 'format', 'schedule', 'curriculum', 'activity', 'learning', 'evaluate', 'other', 'cover', 'gender'];
+    protected $fillable = ['user_id', 'hs_name', 'name', 'student_name', 'birth_date', 'school_exp', 'development', 'part1_label', 'part1', 'part2_label', 'part2', 'part3_label', 'part3', 'part4_label', 'part4', 'other', 'cover', 'gender'];
 
     #protected $guarded = [];//ใช้อันนี้ ถ้าต้องการให้ fillable ทุก field ให้เลือกใช้อย่างใดอย่างหนึ่งกับ $fillable กรณีที่ไม่มี field ใน table แล้วส่งค่ามาก็จะ error กรณีนี้เราทำ json field เลย error ที่ father_name field not found
 		public $hasMany =[
